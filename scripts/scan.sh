7cc35d23cc4e1f43a3bdb1e944d3da71054bcc6a
adhamyaqoub <eng.adhamyaqoub@gmail.com>
2026-03-08 17:40:46 +0200

    L: add social security scanner

--- scripts/scan.sh
+++ scripts/scan.sh
@@ -1,8 +1,16 @@
+#!/bin/bash
+
+# === K: Credit Card Scanner ===
 printf "\n====== SCANNING FOR CREDIT CARD NUMBERS ======\n"
 grep -rE --color=always '(\b[0-9]{4}[- ]?){3}[0-9]{4}\b' . --exclude-dir={.git} --line-number
-echo "========= CREDIT CARD SCAN COMPLETE =========="# TODO: write the script
+echo "========= CREDIT CARD SCAN COMPLETE =========="
 
+# === L: Social Security Number Scanner ===
+printf "\n==== SCANNING FOR SOCIAL SECURITY NUMBERS ====\n"
+grep -rE --color=always '\b[0-9]{3}-[0-9]{2}-[0-9]{4}\b' . --exclude-dir={.git} --line-number
+echo "======= SOCIAL SECURITY SCAN COMPLETE ========"
 
+# === M: Phone Number Scanner ===
 printf "\n========= SCANNING FOR PHONE NUMBERS =========\n"
 grep -rE --color=always '\b[0-9]{3}-[0-9]{3}-[0-9]{4}\b' . --exclude-dir={.git} --line-number
 grep -rE --color=always '\([0-9]{3}\) [0-9]{3}-[0-9]{4}' . --exclude-dir={.git} --line-number
